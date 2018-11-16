d253af99680f3e475cae31503805c30610e79829
Jesse Sosef <44866345+302776692@users.noreply.github.com>
2018-11-16 10:05:16 +0100

    walk animaties voor en naar achter gemaakt

--- Hero.java
+++ Hero.java
@@ -6,13 +6,14 @@ import greenfoot.*;
  * @author R. Springer
  */
 public class Hero extends Mover {
-    
+
     private final double gravity;
     private final double acc;
     private final double drag;
     boolean facingLeft;
     boolean mirror;
-    private GreenfootImage stand;
+    private int teller = 0;
+    private GreenfootImage walk0;
     private GreenfootImage walk1;
     private GreenfootImage walk2;
     private GreenfootImage walk3;
@@ -24,28 +25,27 @@ public class Hero extends Mover {
     private GreenfootImage walk9;
     private GreenfootImage walk10;
     private GreenfootImage walk11;
-
+    GreenfootImage[] walkArray = new GreenfootImage[12];
 
     public Hero() {
         super();
         gravity = 9.8;
         acc = 0.3;
         drag = 0.8;
-        stand = new GreenfootImage("p1.png");
-        walk1 = new GreenfootImage("p1_walk01.png");
-        walk2 = new GreenfootImage("p1_walk02.png");
-        walk3 = new GreenfootImage("p1_walk03.png");
-        walk4 = new GreenfootImage("p1_walk04.png");
-        walk5 = new GreenfootImage("p1_walk05.png");
-        walk6 = new GreenfootImage("p1_walk06.png");
-        walk7 = new GreenfootImage("p1_walk07.png");
-        walk8 = new GreenfootImage("p1_walk08.png");
-        walk9 = new GreenfootImage("p1_walk09.png");
-        walk10 = new GreenfootImage("p1_walk10.png");
-        walk11 = new GreenfootImage("p1_walk11.png");
+        walkArray[0] = new GreenfootImage("p1.png");
+        walkArray[1] = new GreenfootImage("p1_walk01.png");
+        walkArray[2] = new GreenfootImage("p1_walk02.png");
+        walkArray[3] = new GreenfootImage("p1_walk03.png");
+        walkArray[4] = new GreenfootImage("p1_walk04.png");
+        walkArray[5] = new GreenfootImage("p1_walk05.png");
+        walkArray[6] = new GreenfootImage("p1_walk06.png");
+        walkArray[7] = new GreenfootImage("p1_walk07.png");
+        walkArray[8] = new GreenfootImage("p1_walk08.png");
+        walkArray[9] = new GreenfootImage("p1_walk09.png");
+        walkArray[10] = new GreenfootImage("p1_walk10.png");
+        walkArray[11] = new GreenfootImage("p1_walk11.png");
 
-        
-        setImage("p1.png");
+        setImage(walkArray[0]);
     }
 
     @Override
@@ -58,7 +58,7 @@ public class Hero extends Mover {
             velocityY = gravity;
         }
         applyVelocity();
-        
+
         getWorld().showText(getX() + ", " + getY(), 500, 30); 
 
         for (Actor enemy : getIntersectingObjects(Enemy.class)) {
@@ -67,75 +67,38 @@ public class Hero extends Mover {
                 break;
             }
         }
-        
+
         if(isTouching(DangerousTiles.class)){
             setLocation(checkpointX, checkpointY);
         }
-        
+
     }
+
     public void handleInput() {
         if (Greenfoot.isKeyDown("w") && velocityY == 0) {
             velocityY = -10;
         }
 
         if (Greenfoot.isKeyDown("a")) {
-
-            if(getImage() == stand){
-                setImage(walk1);
-            } else if (getImage() == walk1){
-                setImage(walk2);
-            } else if (getImage() == walk2){
-                setImage(walk3);
-            } else if (getImage() == walk3){
-                setImage(walk4);
-            } else if (getImage() == walk4){
-                setImage(walk5);
-            } else if (getImage() == walk5){
-                setImage(walk6);
-            } else if (getImage() == walk6){
-                setImage(walk7);
-            } else if (getImage() == walk7){
-                setImage(walk8);
-            } else if (getImage() == walk8){
-                setImage(walk9);
-            } else if (getImage() == walk9){
-                setImage(walk10);
-            } else if (getImage() == walk10){
-                setImage(walk11);
-            } else {
-                setImage(walk1);
-            }
-
+            mirrorHero();
+            facingLeft = true;
+            walkAnimatie();
             velocityX = -2;
         } else if (Greenfoot.isKeyDown("d")) {
-            if(getImage() == stand){
-                setImage(walk1);
-            } else if (getImage() == walk1){
-                setImage(walk2);
-            } else if (getImage() == walk2){
-                setImage(walk3);
-            } else if (getImage() == walk3){
-                setImage(walk4);
-            } else if (getImage() == walk4){
-                setImage(walk5);
-            } else if (getImage() == walk5){
-                setImage(walk6);
-            } else if (getImage() == walk6){
-                setImage(walk7);
-            } else if (getImage() == walk7){
-                setImage(walk8);
-            } else if (getImage() == walk8){
-                setImage(walk9);
-            } else if (getImage() == walk9){
-                setImage(walk10);
-            } else if (getImage() == walk10){
-                setImage(walk11);
-            } else {
-                setImage(walk1);
-            }
-
+            facingLeft = false;
+            mirrorHero();
+            walkAnimatie();
             velocityX = 2;
+
         }
+    }
+
+    public void walkAnimatie() {
+        setImage(walkArray[teller]);
+        teller++;
+        if(teller >= 11){
+            teller = 0;
+        }  
     }
 
     public int getWidth() {
@@ -146,7 +109,20 @@ public class Hero extends Mover {
         return getImage().getHeight();
     }
 
+    public void mirrorImage() {
+        for(int i = 0; i < walkArray.length; i++){
+            walkArray[i].mirrorHorizontally();
+        }
+    }
+
     public void mirrorHero() {
-        
+        if(facingLeft == false && mirror == true){
+            mirrorImage();
+            mirror = false;
+        }
+        if(facingLeft == true && mirror == false){
+            mirrorImage();
+            mirror = true;
+        }
     }
 }
